4eb5498f75f689fc34a3e8b321ff37f5f1c44082
Jefferson Estevam <jesbmail@gmail.com>
2022-08-19 18:12:06 -0300

    ajustes nos comentários

--- AULA01/src/App.java
+++ AULA01/src/App.java
@@ -6,12 +6,14 @@ public class App {
         //Cria um Objeto do tipo Scanner( scanner ) que recebe o System.in como parâmetro
         //para tratar a entrada de dados
         Scanner scanner = new Scanner(System.in);
+        //Cria um Objeto do tipo Parfum( parfum ) 
+        //para embelezar o programa
         Parfum parfum = new Parfum();
 
         // Variáveis
         String nome = ""; //Variável do tipo string com o valor ""
         int idade = 0; //Variável do tipo int com o valor 0
-        double altura = 0; //Variável do tipo int com o valor 0
+        double altura = 0; //Variável do tipo double com o valor 0
 
         parfum.start();
         System.out.println("Digite o seu nome: ");
@@ -27,14 +29,14 @@ public class App {
         
         System.out.println("Digite a sua altura: ");
         if(scanner.hasNext()){ //Verifica se existe uma entrada de dados
-            altura = scanner.nextDouble(); //Atribui o valor digitado para a variável idade
+            altura = scanner.nextDouble(); //Atribui o valor digitado para a variável altura
         }
         
         parfum.end();
         System.out.println("Nome: "+ nome);
         System.out.println("Idade: "+ idade);
         System.out.println("Altura: "+ altura);
-        
+
         scanner.close();
     }
 }
