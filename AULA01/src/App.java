36d40169059a13eced2d1863785210e91a44739c
Jefferson Estevam <jesbmail@gmail.com>
2022-08-19 18:10:09 -0300

    Melhorias aula 1

--- AULA01/src/App.java
+++ AULA01/src/App.java
@@ -1,32 +1,40 @@
+
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        //Cria um Objeto do tipo Scanner( sc ) que recebe o System.in como parâmetro
+        //Cria um Objeto do tipo Scanner( scanner ) que recebe o System.in como parâmetro
         //para tratar a entrada de dados
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        Parfum parfum = new Parfum();
 
         // Variáveis
         String nome = ""; //Variável do tipo string com o valor ""
         int idade = 0; //Variável do tipo int com o valor 0
+        double altura = 0; //Variável do tipo int com o valor 0
 
-
+        parfum.start();
         System.out.println("Digite o seu nome: ");
-        if(sc.hasNext()){ //Verifica se existe uma entrada de dados
-            nome = sc.nextLine(); //Atribui o valor digitado para a variável nome
+        if(scanner.hasNext()){ //Verifica se existe uma entrada de dados
+            nome = scanner.nextLine(); //Atribui o valor digitado para a variável nome
         }
 
 
         System.out.println("Digite a sua idade: ");
-        if(sc.hasNext()){ //Verifica se existe uma entrada de dados
-            idade = sc.nextInt(); //Atribui o valor digitado para a variável idade
+        if(scanner.hasNext()){ //Verifica se existe uma entrada de dados
+            idade = scanner.nextInt(); //Atribui o valor digitado para a variável idade
         }
         
-        System.out.println(" ===================");
-        System.out.println("| Dados Cadastrados |");
-        System.out.println(" ===================");
+        System.out.println("Digite a sua altura: ");
+        if(scanner.hasNext()){ //Verifica se existe uma entrada de dados
+            altura = scanner.nextDouble(); //Atribui o valor digitado para a variável idade
+        }
+        
+        parfum.end();
         System.out.println("Nome: "+ nome);
         System.out.println("Idade: "+ idade);
-
+        System.out.println("Altura: "+ altura);
+        
+        scanner.close();
     }
 }
---
+++ AULA01/src/Parfum.java
@@ -0,0 +1,13 @@
+public class Parfum {
+    public void start(){
+        System.out.println("=================");
+        System.out.println("Cadastro de dados");
+        System.out.println("=================");
+    }
+
+    public void end(){
+        System.out.println("=================");
+        System.out.println("Listagem de dados");
+        System.out.println("=================");
+    }
+}
